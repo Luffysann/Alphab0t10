094183ee1b30b377406d0b58e181f9f047fbb643
Luffysann <94328513+Luffysann@users.noreply.github.com>
2021-11-15 08:34:03 +0700

    Update sewa.js

--- shop/sewa.js
+++ shop/sewa.js
@@ -1,28 +1,22 @@
-const pc_sewa = (_minggu, __minggu, ___minggu, _bulan, _permanen, __permanen) => {
+const pc_sewa = (_permanen) => {
 return `「 *PRICE LIST* 」
 							
 ⬣ PRICE SEWABOT
 
-⬡ 1 MINGGU = ${_minggu}
-⬡ 2 MINGGU = ${__minggu}
-⬡ 3 MINGGU = ${___minggu}
-⬡ 1 BULAN = ${_bulan}
 ⬡ PERMANEN = ${_permanen}
-⬡ PERMANEN + USER PREM = ${__permanen}
 
 
 ⬣ PRICE JADIBOT
 
-⬡ JADI BOT PERMANEN 25K
+⬡ JADI BOT PERMANEN 20K
 
 
 ⬣ MINAT? PM
-⬡ wa.me/62887435047326
+⬡ wa.me/62882397144534
 
 
 NOTE : 
-Bot On 24 Jam Karna Di Run Menggunakan Heroku
-Reset Data Setiap Jam 02:00 `
+Bot On 24 Jam `
 	}
 
 exports.pc_sewa = pc_sewa
